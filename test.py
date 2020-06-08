28405f55d3261c97a2fdeb706c8f0a47d5a47b75
Ananta Srikar <srikarananta01@gmail.com>
2020-06-08 13:29:27 +0530

    Added test case for CI

---
+++ test.py
@@ -0,0 +1,16 @@
+from app import app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+
+	# Checking for correct flask setup
+
+	def test_home(self):
+		
+		tester = app.test_client(self)
+		# response = tester.get('/', follow_redirects = True)
+		response = tester.get('/', content_type = 'html/text')
+		self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+	unittest.main()
