cc61fda3613447f514cef5c95f8db7d1a130ca98
Ananta Srikar <srikarananta01@gmail.com>
2020-07-01 16:06:14 +0530

    Fixed small bug

--- Hforms/forms.py
+++ Hforms/forms.py
@@ -4,7 +4,7 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from Hforms.dbModels import User
 
 class RegistrationForm(FlaskForm):
@@ -20,7 +20,7 @@ class RegistrationForm(FlaskForm):
 			raise ValidationError('Sorry! Thats username is already taken. Try another one.')
 
 class LoginForm(FlaskForm):
-	username = StringField('Username', validators = [DataRequired(), Email()])
+	username = StringField('Username', validators = [DataRequired()])
 	password = PasswordField('Password', validators = [DataRequired()])
 	remember = BooleanField('Remember me')
 	submit = SubmitField('Login')
