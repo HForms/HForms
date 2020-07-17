67ffc3ac706330836b55ead8014241fbb2ba84b0
Dev <devvmashru@protonmail.ch>
2020-07-17 13:20:22 +0530

    Remove unnecessary column

--- Hforms/dbModels.py
+++ Hforms/dbModels.py
@@ -9,7 +9,6 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key = True)
 	username = db.Column(db.String(20), unique = True, nullable = False)
-	image_file = db.Column(db.String(20), nullable = False, default = 'default.jpeg')
 	password = db.Column(db.String(60), nullable = False)
 
 	def __repr__(self):
